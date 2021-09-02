62cbb812a250e5f250bf8a4c84de4dd06a205ccc
Sun <sunyoungkim.dev@gmail.com>
2021-09-02 19:29:12 +0900

    day 11

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,69 @@
+/* Get our elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+/* Build out functions */
+function togglePlay() {
+    // video has paused property(not playing)
+    // if (video.paused) {
+    //     video.play();
+    // } else {
+    //     video.pause();
+    // }
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    const seconds = parseFloat(this.dataset.skip); // parseFloat() : string => number
+    video.currentTime += seconds;
+    console.log(seconds);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+    // console.log(this.name);
+    // console.log(this.value);
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+    // (클릭한 위치의 x좌표 값 / progress 전체 너비) * 비디오 길이
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(scrubTime);
+}
+
+/* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);   // progress 이벤트도 있음
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(input => input.addEventListener('change', handleRangeUpdate));
+ranges.forEach(input => input.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;  // flag
+progress.addEventListener('click', scrub);
+// mousedown true 일 때만 실행, mousemove 시 initial event 전달 필요
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e)); 
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+// requestFullscreen 사용해서 full screen button 만들어보기
